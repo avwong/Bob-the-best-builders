b84f5bb9a621ff8d15ebb1f8c5209ecb8d83405e
FabianSanchezD <fabiansanchezd@outlook.com>
2026-05-16 11:39:35 -0600

    fix: connection to DB was not wokring

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -6,7 +7,12 @@ import { ProductsModule } from './products/products.module';
 import { SupermarketsModule } from './supermarkets/supermarkets.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, SupermarketsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    ProductsModule,
+    SupermarketsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
